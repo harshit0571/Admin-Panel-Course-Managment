aec56383ebac465775c53190ff277a5787ce1b7c
harshit0571 <harshitchadha65@gmail.com>
2024-03-12 14:42:35 +0530

    api changed

--- app/payment/page.js
+++ app/payment/page.js
@@ -74,7 +74,7 @@ function Product() {
     console.log(order);
 
     var options = {
-      key: "rzp_test_ITafrUIVpjBMpW", // Enter the Key ID generated from the Dashboard
+      key: "keyid", // Enter the Key ID generated from the Dashboard
       amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
       currency,
       name: "Courses For Career", //your business name
